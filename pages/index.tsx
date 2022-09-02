a71cbd01a5529716b40ee1424119930b54cd2281
yassinrais <yassmailrais@gmail.com>
2022-09-02 14:32:31 +0000

    feat: lets go

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,25 @@
 import Head from 'next/head'
-import { GetStaticProps, NextPage } from 'next'
-import Post from '../components/post'
-import { PostData, PostDataListProps } from '../types/postdata'
-import { GetPosts } from '../lib/postdata_api'
+import { NextPage } from 'next'
 
-export const getStaticProps: GetStaticProps = async (_context) => {
-  // fetch list of posts
-  const posts: PostData[] = await GetPosts()
-  return {
-    props: {
-      postDataList: posts,
-    },
-  }
-}
+const IndexPage: NextPage = () => {
 
-const IndexPage: NextPage<PostDataListProps> = ({
-  postDataList,
-}: PostDataListProps) => {
   return (
     <main>
       <Head>
-        <title>Home page</title>
+        <title>Nude.js</title>
       </Head>
 
-      <h1>List of posts</h1>
-
-      <section>
-        {postDataList.map((post: PostData) => (
-          <Post {...post} key={post.id} />
-        ))}
-      </section>
+      <h1 className="hidden">Nudejs.dev</h1>
+      <div className="container">
+        <a className="prank shake" href="https://github.com/yassinrais/">
+          <img className="prank" src="https://avatars.githubusercontent.com/u/25354731?v=4" alt="NudeJs Logo" />
+          <h1 className="gelatine">😈 NoooNooo !</h1>
+        </a>
+        <div className="nudejs">
+          <h1>👇👇👇 👇👇👇</h1>
+          <img src="/nudejs.png" alt="NudeJs Logo" />
+        </div>
+      </div>
     </main>
   )
 }
